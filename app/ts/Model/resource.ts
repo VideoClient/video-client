06969b06af6b956e77fe78b76190270b27abca31
sxf <sunxfancy@gmail.com>
2017-02-01 15:44:02 +0800

    combine search

--- app/ts/Model/resource.ts
+++ app/ts/Model/resource.ts
@@ -1,7 +1,70 @@
+import {ISearchAdapter, IClassifiedVideoAdapter} from '.'
 
-export class ResourceLoader {
+export class ResourceLoader implements ISearchAdapter, IClassifiedVideoAdapter {
+    search_adapters: ISearchAdapter[]
+    classified_adapters: IClassifiedVideoAdapter[]
+
+    regSearchAdapter(searcher: ISearchAdapter) {
+        this.search_adapters.push(searcher)
+    }
+
+    regClassifiedAdapter(classified: IClassifiedVideoAdapter) {
+        this.classified_adapters.push(classified)
+    }
+
+
+    search_video(name: string, page: number, count?: number):Promise<Video[]>  // 视频
+    {
+        return this.search_basic<Video>('search_video', name, page, count)
+    }
+
+    search_album(name: string, page: number, count?: number):Promise<VideoCollection[]> // 专辑
+    {
+        return this.search_basic<VideoCollection>('search_album', name, page, count)
+    }
+
+    search_show(name: string, page: number, count?: number):Promise<VideoCollection[]> // 节目
+    {
+        return this.search_basic<VideoCollection>('search_show', name, page, count)
+    }
+
     
+    search_basic<T>(funcname: string, name: string, page: number, count?: number):Promise<T[]>
+    {
+        return new Promise<T[]>(async (resolve, reject) => {
+            let ret:Promise<T[]>[] = []
+            for (let i of this.search_adapters) {
+                 ret.push(i[funcname](name, page, count / this.search_adapters.length))
+            }
+            let ans:T[] = []
+            for (let i in ret) {
+                let data = await ret[i]
+                ans.concat(data)
+            }
+            resolve(ans)
+        })
+    }
 
+    get_video(classname: string, page: number, count?: number):Promise<Video[]>
+    {
+        return new Promise<Video[]>((resolve, reject) => {
+
+        })
+    }
+
+    get_album(classname: string, page: number, count?: number):Promise<VideoCollection[]> 
+    {
+        return new Promise<VideoCollection[]>((resolve, reject) => {
+            
+        })
+    }
+    
+    get_show(classname: string, page: number, count?: number):Promise<VideoCollection[]> 
+    {
+        return new Promise<VideoCollection[]>((resolve, reject) => {
+            
+        })
+    }
  
 }
 
